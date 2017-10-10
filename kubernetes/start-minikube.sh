cb2fcf792612a889a4ba65e4d9f63939e4383ba2
rubengeertsema <rubengeertsema@gmail.com>
2017-10-11 01:23:19 +0200

    Fix for linux

--- kubernetes/start-minikube.sh
+++ kubernetes/start-minikube.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-function start_minikube() {
-    cpus=4
-    memory=8192
-
-    printf "\n---- Starting local Kubernetes cluster ----\n"
-    printf "Starting VM with $cpus cpus and $memory memory...\n"
-    printf "If needed, change these settings in [./kubernetes/start-minikube.sh]\n"
-    minikube config set cpus $cpus
-    minikube config set memory $memory
-    minikube start --vm-driver=virtualbox
-}
+# VirtualBox settings
+cpus=4
+memory=8192
 
 function wait_for_minikube_readiness() {
     printf "\n---- Waiting for local cluster to be ready ----\n"
@@ -27,6 +19,9 @@ function apply_descriptor() {
     done
 }
 
-start_minikube
+printf "\n---- Starting local Kubernetes cluster ----\n"
+printf "Starting VM with $cpus cpus and $memory memory...\n"
+printf "If needed, change these settings in [./kubernetes/start-minikube.sh]\n"
+minikube start --memory=${memory} --cpus=${cpus} --vm-driver=virtualbox
 wait_for_minikube_readiness
 apply_descriptor traefik.yml namespaces/tools.yml namespaces/prod.yml
